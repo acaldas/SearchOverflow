11ad269c1bfc4bd67b3a6084049d8fac1270285a
Vitor Mota <vitormanuelmotasilva@gmail.com>
2014-11-05 16:22:59 +0000

    index

--- routes/socket.js
+++ routes/socket.js
@@ -6,24 +6,24 @@
 var solrClient = null;
 
 exports.socket = function socket(socket) {
-    socket.on('solr:ask', function() {
+    socket.on('solr:ask', function () {
         if (solrClient) {
             socket.emit('solr:ready');
         }
     });
 
-    socket.on('solr:query', function(query) {
+    socket.on('solr:query', function (query) {
         var promise = solrClient.querySolr(query);
 
-        promise.then(function(result) {
+        promise.then(function (result) {
             socket.emit('solr:queryResult', result);
-        }
+        });
     });
 };
 
-exports.solrReady= function(solr) {
+exports.solrReady = function (solr) {
     solrClient = solr;
-    if(solrClient)
+    if (solrClient)
         socket.emit('solr:ready');
 }
 
